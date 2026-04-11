ba976ccead36808a2162ce07719c5a569eca9ef0
danozzi <danozzimedia@gmail.com>
2026-04-11 21:35:00 +1000

    Fix Resend email error handling

--- src/lib/email.js
+++ src/lib/email.js
@@ -3,6 +3,8 @@ const { Resend } = require('resend')
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 async function sendEmail(to, subject, html) {
+  console.log("🔥 EMAIL FUNCTION ENTERED") // 👈 ADD THIS HERE
+
   try {
     const { data, error } = await resend.emails.send({
       from: process.env.RESEND_FROM_EMAIL,
